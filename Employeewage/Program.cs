13da370e65eac11478afbdf582baa00f811d105c
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-01 19:08:17 +0530

    [Akshay]-Add Part time employee wage

--- Employeewage/Program.cs
+++ Employeewage/Program.cs
@@ -6,17 +6,23 @@ namespace Employeewage
     {
         static void Main(string[] args)
         {
-            int fullTime = 1, Emp_Rate_Per_Hour = 20, empHrs = 0, empWage = 0;
+            int fullTime = 1,partTime = 2, Emp_Rate_Per_Hour = 20, empHrs = 0, empWage = 0;
             Random randobj = new Random();
-            int checkPresent = randobj.Next(0, 2);
+            int checkPresent = randobj.Next(0, 3);
             Console.WriteLine("Randome number=" + checkPresent);
-            if (checkPresent == fullTime)
+            if(checkPresent == partTime) 
+            {
+                empHrs = 4;
+                Console.WriteLine("Employee is present part time");
+            }
+
+            else if (checkPresent == fullTime)
             {
                 empHrs = 8;
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("Employee is present full time");
 
             }
-            else
+            else 
             {
                 empHrs = 0;
                 Console.WriteLine("Employee is absent");
