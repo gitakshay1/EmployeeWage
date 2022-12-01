b9ae69142ce7a9f04cca019d03eb6a4811024a5a
gitakshay1 <akshaypatil309p8@gmail.com>
2022-12-01 18:16:16 +0530

    [Akshay];Employee is present or absent

---
+++ Employeewage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Employeewage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fullTime = 1;
+            Random randobj = new Random();
+            int checkPresent = randobj.Next(0, 2);
+            if (checkPresent == fullTime)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
+        }
+    }
+}
